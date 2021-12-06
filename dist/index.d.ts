211483094b376e051e3887e5fd1b8592379f98f6
nge6427 <6220252+nge6427@users.noreply.github.com>
2021-12-06 16:11:28 -0500

    Add dist folder

---
+++ dist/index.d.ts
@@ -0,0 +1,1 @@
+export { mainnet } from "./mainnet";
---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.mainnet = void 0;
+//export { devnet } from "./devnet";
+var mainnet_1 = require("./mainnet");
+Object.defineProperty(exports, "mainnet", { enumerable: true, get: function () { return mainnet_1.mainnet; } });
+//export { testnet } from "./testnet";
+//# sourceMappingURL=index.js.map
